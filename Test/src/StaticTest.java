ba3debb60d9de6894559782ef1b86985ed659806
hanjiayu <13390934591@163.com>
2015-09-11 10:23:42 +0800

    Static的使用

---
+++ Test/src/StaticTest.java
@@ -0,0 +1,18 @@
+
+public class StaticTest {
+	public static void info(){//实例
+		System.out.println("输出info了");
+	}
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {//类
+		// TODO Auto-generated method stub
+		info();//实例
+	}
+	//有static修饰的就相当于人类
+	//没有static修饰的就相当于小红
+	//在static修饰的方法中用this，就相当于人类的吃
+	//所以吃的方法也得用人类修饰。
+
+}
